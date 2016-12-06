ae03d4665c50a611ad565107a069a5b06ef88ac6
Hyman_Chen <hyman_chen@wistron.com>
2016-12-06 14:12:55 +0800

    initial project

---
+++ nightwatch.conf.js
@@ -0,0 +1,13 @@
+var config = {
+  "selenium" : {
+    "start_process" : true,
+    "server_path" : "",
+    "log_path" : "",
+    "port" : 4444,
+    "cli_args" : {
+      "webdriver.chrome.driver" : "bin/chromedriver_mac"
+    }
+  }
+}
+
+module.exports = config;
