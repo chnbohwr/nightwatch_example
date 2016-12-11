20b6390ebe7baaa11f7afa44fa12f92360710884
chnbohwr <chnbohwr@gmail.com>
2016-12-11 20:37:49 +0800

    finish test case

--- test/test1.js
+++ test/test1.js
@@ -8,13 +8,22 @@ module.exports = {
       .setValue('input#login_field', 'fastfood001')
       .setValue('input#password', 'fastfood000')
       .click('input.btn')
-      .pause(5000)
+      .pause(3000)
       .click('.header-nav-link.tooltipped.tooltipped-s.js-menu-target')
       .pause(500)
       .click('.dropdown-item:first-child')
       .pause(1000)
-      .setValue('#repository_name'. 'test_repository')
+      .setValue('#repository_name', 'test_repository')
       .click('.btn.btn-primary.first-in-line')
+      .pause(3000)
+      .click('#js-repo-pjax-container > div.pagehead.repohead.instapaper_ignore.readability-menu.experiment-repo-nav > div:nth-child(2) > nav > a:nth-child(8)')
+      .pause(3000)
+      .click('#options_bucket > div.boxed-group.dangerzone > div > button:nth-child(11)')
+      .pause(1000)
+      .setValue('#facebox > div > div > form > p > input', 'test_repository')
+      .click('#facebox > div > div > form > button')
+      .pause(5000)
       .end();
+
   }
 };
